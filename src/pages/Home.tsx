653a2357264726cf417be06d38ac278fc4007c9d
Luis Felipe Olimpio <lipeolimpio12@gmail.com>
2021-09-30 19:38:03 -0300

    feat: changes to submit the challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(prevState => [...prevState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const taskExists = tasks.some(task => task.id === id);
+
+    if(!taskExists) {
+      return;
+    }
+
+    setTasks(prevState => prevState.map(task => task.id === id ? { ...task, done: !task.done } : task));
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(prevState => prevState.filter(task => task.id !== id));
   }
 
   return (
